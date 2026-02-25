735dd6f284470de11473945bf4afa912fbe64340
Adın Soyadın <mailadresin@gmail.com>
2026-02-25 23:37:22 +0300

    ssssssssssssssssssssssssss

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -36,9 +36,9 @@ export default function HeroSection() {
   ];
 
   const slideVariants = {
-    enter: (direction) => ({ x: direction > 0 ? 50 : -50, opacity: 0 }),
+    enter: (direction: number) => ({ x: direction > 0 ? 50 : -50, opacity: 0 }),
     center: { zIndex: 1, x: 0, opacity: 1 },
-    exit: (direction) => ({
+    exit: (direction: number) => ({
       zIndex: 0,
       x: direction < 0 ? 50 : -50,
       opacity: 0,
